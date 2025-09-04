80cbaa82b976e5eb0dc3cf8778b8697b2ef335ae
Berkay <146650666+berkaydemircin@users.noreply.github.com>
2025-09-04 13:56:53 +0300

    Implemented circuit breaker for orders

--- order-service/src/main/java/com/berkayd/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/berkayd/microservices/order/config/RestClientConfig.java
@@ -1,8 +1,13 @@
 package com.berkayd.microservices.order.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -17,10 +22,17 @@ public class RestClientConfig {
 
     @Bean
     public InventoryClient inventoryClient() {
-        RestClient rest = RestClient.builder().baseUrl(inventoryServiceUrl).build();
+        RestClient rest = RestClient.builder().baseUrl(inventoryServiceUrl).requestFactory(getClientRequestFactory()).build();
 
         RestClientAdapter rca = RestClientAdapter.create(rest);
         HttpServiceProxyFactory hspf = HttpServiceProxyFactory.builderFor(rca).build();
         return hspf.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        ClientHttpRequestFactorySettings s = ClientHttpRequestFactorySettings.DEFAULTS
+            .withConnectTimeout(Duration.ofSeconds(3))
+            .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(s);
     }
 }
