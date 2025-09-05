4e402f737474b163876d290554bafb95b8812e7a
Berkay <146650666+berkaydemircin@users.noreply.github.com>
2025-09-05 06:06:03 +0300

    Added skuCode field to product*
    And all related methods

--- product-service/src/main/java/com/berkayd/microservices/product/model/Product.java
+++ product-service/src/main/java/com/berkayd/microservices/product/model/Product.java
@@ -20,5 +20,6 @@ public class Product {
     private String id;
     private String description;
     private String name;
+    private String skuCode;
     private BigDecimal price;
 }
--- product-service/src/main/java/com/berkayd/microservices/product/service/ProductService.java
+++ product-service/src/main/java/com/berkayd/microservices/product/service/ProductService.java
@@ -19,10 +19,10 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     public ProductResponse createProduct(ProductRequest productRequest) {
-        Product product = Product.builder().name(productRequest.name()).description(productRequest.description()).price(productRequest.price()).build();
+        Product product = Product.builder().name(productRequest.name()).description(productRequest.description()).skuCode(productRequest.skuCode()).price(productRequest.price()).build();
         productRepository.save(product);
         log.info("Product created successfully");
-        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getSkuCode(), product.getPrice());
     }
 
     public List<Product> getAllProducts() {
