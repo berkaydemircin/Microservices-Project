c437b266d13b03290cc1fa7246db51ece9831961
Berkay <146650666+berkaydemircin@users.noreply.github.com>
2025-09-07 17:30:23 +0300

    Configured basic kafka support

--- order-service/src/main/java/com/berkayd/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/berkayd/microservices/order/service/OrderService.java
@@ -2,11 +2,15 @@ package com.berkayd.microservices.order.service;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.berkayd.microservices.order.client.InventoryClient;
 import com.berkayd.microservices.order.dto.OrderRequest;
 import com.berkayd.microservices.order.model.Order;
+import com.berkayd.microservices.order.event.OrderPlacedEvent;
 import com.berkayd.microservices.order.repository.OrderRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +21,9 @@ public class OrderService {
     
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
+
 
     public void placeOrder(OrderRequest orderRequest) {
         boolean flag = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -29,6 +36,11 @@ public class OrderService {
             order.setQuantity(orderRequest.quantity());
 
             orderRepository.save(order);
+
+            OrderPlacedEvent e = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Started sending OrderPlacedEvent {} to topic order-placed", e);
+            kafkaTemplate.send("order-placed", e);
+            log.info("Ended sending OrderPlacedEvent {} to topic order-placed", e);
         }
         else {
             throw new RuntimeException("Product wth sku code " + orderRequest.skuCode() + " is not in stock");
